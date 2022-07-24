0ed3bcd42452dfed7953000cd3f020c00e44e411
kotaro210 <kotaros@kotaronoMacBook-Air.local>
2022-07-24 13:14:04 +0900

    pythonのフォルダを追加した

---
+++ api/main.py
@@ -0,0 +1,36 @@
+from flask import Flask, request, jsonify
+
+from googletrans import Translator
+import re
+
+app = Flask(__name__)
+app.config["JSON_AS_ASCII"] = False
+
+
+
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
+
+
+@app.route('/translate', methods=["GET"])
+def document_translate():
+
+    # クエリのパラメータの取得
+    req = request.args
+    document = req.get("doc")
+    translator = Translator()
+
+    # 英数字の場合の正規表現パターン定義
+    regexp = re.compile(r"[\da-zA-Z]+")
+
+    if re.match(regexp, document) is not None:
+        message = translator.translate(document, src='en', dest='ja')
+        return jsonify(message.text)
+    else:
+        message = translator.translate(document, src='ja', dest='en')
+        return jsonify(message.text)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
